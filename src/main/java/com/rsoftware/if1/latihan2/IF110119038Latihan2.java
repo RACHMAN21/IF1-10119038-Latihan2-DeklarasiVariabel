ef41d39ea7aa112153b2ffc687704d40e7cc567a
Rachman Aldiansyah <rachmanaldiansyah03@gmail.com>
2020-10-16 14:56:27 +0700

    Upload Project

---
+++ src/main/java/com/rsoftware/if1/latihan2/IF110119038Latihan2.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rsoftware.if1.latihan2;
+
+/**
+ *
+ * @author
+ * NAMA  : Rachman Aldiansyah
+ * KELAS : IF-1
+ * NIM   : 10119038
+ * Deskripsi Program : Program ini berisi untuk deklarasi & pengaksesan variabel
+ * 
+ */
+
+public class IF110119038Latihan2 {
+    public static void main(String[] args) {
+        // Deklarasi variabel
+        int nilaiInt;
+        final double PHI = 3.14; // Konstanta Uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        // Memberi nilai ke variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        // Menampilkan hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai    = " + nilaiInt);
+        System.out.println("Isi variabel PHI      = " + PHI);
+        System.out.println("Isi variabel Logika   = " + nilaiLogika);
+        System.out.println("Isi variabel Karakter = " + nilaiKarakter);
+    }
+}
